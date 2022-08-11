8104119e802e1cb3edc5618d1c1da62540699cf3
Celio Junior <celio_cjr@hotmail.com>
2022-08-11 01:23:43 -0300

    installing nodemon

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
   
-})
+  //Verificação da URL
+  if(request.url === "/") {
+    if(request.method === "GET")
+    return response.end("App is on!");
+  }
+});
 
-server.listen(4080, () => console.log("Server is in PORT 4080"))
+server.listen(4080, () => console.log("Server is in PORT 4080"));
