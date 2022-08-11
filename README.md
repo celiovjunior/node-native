b64ba3342739553aeba1fe49abcfa9b912581383
Celio Junior <celio_cjr@hotmail.com>
2022-08-11 01:13:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto em contrução para estudos.
+Desenvolvendo um servidor simples utilizando NodeJS puro e funções já existentes na ferramenta.
---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  
+})
+
+server.listen(4080, () => console.log("Server is in PORT 4080"))
