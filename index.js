67cb04dfce235c125601eddcc6599e45913ec8d6
Celio Junior <celio_cjr@hotmail.com>
2022-08-11 02:04:08 -0300

    finish

--- index.js
+++ index.js
@@ -23,6 +23,30 @@ const server = http.createServer((request, response) => {
       }).on("end", () => {
         return response.end(JSON.stringify(users));
       });
+    }
+  }
+
+  if(request.url.startsWith("/users")) {
+    if(request.method === "PUT") {
+
+      const url = request.url
+      const splitURL = url.split("/");
+
+      const idUser = splitURL[2];
+
+      const userIndex = users.findIndex(user => user.id === idUser);
+
+      request
+        .on("data", (data) => {
+          const dataUser = JSON.parse(data);
+
+          users[userIndex] = {
+            id: idUser,
+            ...dataUser
+          }
+        }).on("end", () => {
+          return response.end(JSON.stringify(users))
+        });
 
     }
   }
